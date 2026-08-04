1f4119f33e5b7892f040303a6810383525e1d17b
dexter-cnx <73522389+dexter-cnx@users.noreply.github.com>
2026-08-04 13:55:06 +0700

    test: cover PixelCraft home screen

---
+++ test/ui/home_screen_test.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:pixelcraft/ui/screens/home_screen.dart';
+
+void main() {
+  testWidgets('shows samples and gallery import action', (tester) async {
+    await tester.pumpWidget(const MaterialApp(home: HomeScreen()));
+    await tester.pumpAndSettle();
+
+    expect(find.text('PixelCraft'), findsOneWidget);
+    expect(find.text('Edit locally. Move fast.'), findsOneWidget);
+    expect(find.text('Import from Gallery'), findsOneWidget);
+    expect(find.byType(Image), findsNWidgets(4));
+  });
+}
