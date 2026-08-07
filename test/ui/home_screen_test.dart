c4782fb130c5efd6e5320eccf1f8587fa5594641
dexter-cnx <73522389+dexter-cnx@users.noreply.github.com>
2026-08-07 19:18:04 +0700

    test(camera): update home action expectation

--- test/ui/home_screen_test.dart
+++ test/ui/home_screen_test.dart
@@ -3,13 +3,13 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:pixelcraft/ui/screens/home_screen.dart';
 
 void main() {
-  testWidgets('shows samples and gallery import action', (tester) async {
+  testWidgets('shows samples and add photo action', (tester) async {
     await tester.pumpWidget(const MaterialApp(home: HomeScreen()));
     await tester.pumpAndSettle();
 
     expect(find.text('Pixel Craft'), findsOneWidget);
     expect(find.text('Edit locally. Move fast.'), findsOneWidget);
-    expect(find.text('Import from Gallery'), findsOneWidget);
+    expect(find.text('Add Photo'), findsOneWidget);
     expect(find.byType(Image), findsNWidgets(4));
   });
 }
