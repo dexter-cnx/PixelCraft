8a60f2da8d069f8394857a03c1252e5ab0ff637a
dexter-cnx <73522389+dexter-cnx@users.noreply.github.com>
2026-08-07 19:15:00 +0700

    test(camera): cover image source chooser

---
+++ test/ui/home_camera_source_test.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:pixelcraft/ui/screens/home_screen.dart';
+
+void main() {
+  testWidgets('Add Photo offers camera and gallery sources', (tester) async {
+    await tester.pumpWidget(
+      MaterialApp(
+        theme: ThemeData(useMaterial3: true),
+        home: const HomeScreen(),
+      ),
+    );
+    await tester.pumpAndSettle();
+
+    expect(find.text('Add Photo'), findsOneWidget);
+    await tester.tap(find.text('Add Photo'));
+    await tester.pumpAndSettle();
+
+    expect(find.text('Take a photo'), findsOneWidget);
+    expect(find.text('Capture with the device camera'), findsOneWidget);
+    expect(find.text('Choose from gallery'), findsOneWidget);
+    expect(find.text('Open an existing image on this device'), findsOneWidget);
+  });
+}
