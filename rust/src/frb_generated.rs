9edf6ba4032348df466074a58320474a1de5e2f1
Kitipong Sarajan <cnxcoder@gmail.com>
2026-08-07 14:38:23 +0700

    feat(editor): add apply edits and generate filter previews implementations

--- rust/src/frb_generated.rs
+++ rust/src/frb_generated.rs
@@ -38,7 +38,7 @@ flutter_rust_bridge::frb_generated_boilerplate!(
     default_rust_auto_opaque = RustAutoOpaqueMoi,
 );
 pub(crate) const FLUTTER_RUST_BRIDGE_CODEGEN_VERSION: &str = "2.12.0";
-pub(crate) const FLUTTER_RUST_BRIDGE_CODEGEN_CONTENT_HASH: i32 = -186001782;
+pub(crate) const FLUTTER_RUST_BRIDGE_CODEGEN_CONTENT_HASH: i32 = 1544485278;
 
 // Section: executor
 
@@ -74,6 +74,35 @@ fn wire__crate__api__apply_crop_impl(
             deserializer.end();
             transform_result_sse::<_, String>((move || {
                 let output_ok = crate::api::apply_crop(api_x, api_y, api_width, api_height)?;
+                Ok(output_ok)
+            })())
+        },
+    )
+}
+fn wire__crate__api__apply_edits_impl(
+    ptr_: flutter_rust_bridge::for_generated::PlatformGeneralizedUint8ListPtr,
+    rust_vec_len_: i32,
+    data_len_: i32,
+) -> flutter_rust_bridge::for_generated::WireSyncRust2DartSse {
+    FLUTTER_RUST_BRIDGE_HANDLER.wrap_sync::<flutter_rust_bridge::for_generated::SseCodec, _>(
+        flutter_rust_bridge::for_generated::TaskInfo {
+            debug_name: "apply_edits",
+            port: None,
+            mode: flutter_rust_bridge::for_generated::FfiCallMode::Sync,
+        },
+        move || {
+            let message = unsafe {
+                flutter_rust_bridge::for_generated::Dart2RustMessageSse::from_wire(
+                    ptr_,
+                    rust_vec_len_,
+                    data_len_,
+                )
+            };
+            let mut deserializer =
+                flutter_rust_bridge::for_generated::SseDeserializer::new(message);
+            deserializer.end();
+            transform_result_sse::<_, String>((move || {
+                let output_ok = crate::api::apply_edits()?;
                 Ok(output_ok)
             })())
         },
@@ -345,6 +374,42 @@ fn wire__crate__api__flip_vertical_impl(
             deserializer.end();
             transform_result_sse::<_, String>((move || {
                 let output_ok = crate::api::flip_vertical()?;
+                Ok(output_ok)
+            })())
+        },
+    )
+}
+fn wire__crate__api__generate_filter_previews_impl(
+    ptr_: flutter_rust_bridge::for_generated::PlatformGeneralizedUint8ListPtr,
+    rust_vec_len_: i32,
+    data_len_: i32,
+) -> flutter_rust_bridge::for_generated::WireSyncRust2DartSse {
+    FLUTTER_RUST_BRIDGE_HANDLER.wrap_sync::<flutter_rust_bridge::for_generated::SseCodec, _>(
+        flutter_rust_bridge::for_generated::TaskInfo {
+            debug_name: "generate_filter_previews",
+            port: None,
+            mode: flutter_rust_bridge::for_generated::FfiCallMode::Sync,
+        },
+        move || {
+            let message = unsafe {
+                flutter_rust_bridge::for_generated::Dart2RustMessageSse::from_wire(
+                    ptr_,
+                    rust_vec_len_,
+                    data_len_,
+                )
+            };
+            let mut deserializer =
+                flutter_rust_bridge::for_generated::SseDeserializer::new(message);
+            let api_image_bytes = <Vec<u8>>::sse_decode(&mut deserializer);
+            let api_filter_names = <Vec<String>>::sse_decode(&mut deserializer);
+            let api_max_edge = <u32>::sse_decode(&mut deserializer);
+            deserializer.end();
+            transform_result_sse::<_, String>((move || {
+                let output_ok = crate::api::generate_filter_previews(
+                    api_image_bytes,
+                    api_filter_names,
+                    api_max_edge,
+                )?;
                 Ok(output_ok)
             })())
         },
@@ -783,6 +848,18 @@ impl SseDecode for f32 {
     }
 }
 
+impl SseDecode for crate::api::FilterPreviewImage {
+    // Codec=Sse (Serialization based), see doc to use other codecs
+    fn sse_decode(deserializer: &mut flutter_rust_bridge::for_generated::SseDeserializer) -> Self {
+        let mut var_name = <String>::sse_decode(deserializer);
+        let mut var_bytes = <Vec<u8>>::sse_decode(deserializer);
+        return crate::api::FilterPreviewImage {
+            name: var_name,
+            bytes: var_bytes,
+        };
+    }
+}
+
 impl SseDecode for Vec<String> {
     // Codec=Sse (Serialization based), see doc to use other codecs
     fn sse_decode(deserializer: &mut flutter_rust_bridge::for_generated::SseDeserializer) -> Self {
@@ -790,6 +867,18 @@ impl SseDecode for Vec<String> {
         let mut ans_ = Vec::with_capacity(len_ as usize);
         for idx_ in 0..len_ {
             ans_.push(<String>::sse_decode(deserializer));
+        }
+        return ans_;
+    }
+}
+
+impl SseDecode for Vec<crate::api::FilterPreviewImage> {
+    // Codec=Sse (Serialization based), see doc to use other codecs
+    fn sse_decode(deserializer: &mut flutter_rust_bridge::for_generated::SseDeserializer) -> Self {
+        let mut len_ = <i32>::sse_decode(deserializer);
+        let mut ans_ = Vec::with_capacity(len_ as usize);
+        for idx_ in 0..len_ {
+            ans_.push(<crate::api::FilterPreviewImage>::sse_decode(deserializer));
         }
         return ans_;
     }
@@ -895,28 +984,30 @@ fn pde_ffi_dispatcher_sync_impl(
     // Codec=Pde (Serialization + dispatch), see doc to use other codecs
     match func_id {
         1 => wire__crate__api__apply_crop_impl(ptr, rust_vec_len, data_len),
-        2 => wire__crate__api__apply_filter_impl(ptr, rust_vec_len, data_len),
-        3 => wire__crate__api__apply_filter_timed_impl(ptr, rust_vec_len, data_len),
-        4 => wire__crate__api__begin_filter_impl(ptr, rust_vec_len, data_len),
-        5 => wire__crate__api__cancel_filter_impl(ptr, rust_vec_len, data_len),
-        6 => wire__crate__api__commit_filter_impl(ptr, rust_vec_len, data_len),
-        7 => wire__crate__api__current_image_impl(ptr, rust_vec_len, data_len),
-        8 => wire__crate__api__export_image_impl(ptr, rust_vec_len, data_len),
-        9 => wire__crate__api__flip_horizontal_impl(ptr, rust_vec_len, data_len),
-        10 => wire__crate__api__flip_vertical_impl(ptr, rust_vec_len, data_len),
-        11 => wire__crate__api__get_histogram_impl(ptr, rust_vec_len, data_len),
-        12 => wire__crate__api__load_image_impl(ptr, rust_vec_len, data_len),
-        13 => wire__crate__api__original_preview_impl(ptr, rust_vec_len, data_len),
-        14 => wire__crate__api__photon_filter_names_impl(ptr, rust_vec_len, data_len),
-        15 => wire__crate__api__prepare_preview_impl(ptr, rust_vec_len, data_len),
-        16 => wire__crate__api__redo_impl(ptr, rust_vec_len, data_len),
-        17 => wire__crate__api__resize_committed_impl(ptr, rust_vec_len, data_len),
-        18 => wire__crate__api__resize_image_impl(ptr, rust_vec_len, data_len),
-        19 => wire__crate__api__rotate_quarter_turns_impl(ptr, rust_vec_len, data_len),
-        20 => wire__crate__api__session_info_impl(ptr, rust_vec_len, data_len),
-        21 => wire__crate__api__straighten_impl(ptr, rust_vec_len, data_len),
-        22 => wire__crate__api__undo_impl(ptr, rust_vec_len, data_len),
-        23 => wire__crate__api__update_filter_preview_impl(ptr, rust_vec_len, data_len),
+        2 => wire__crate__api__apply_edits_impl(ptr, rust_vec_len, data_len),
+        3 => wire__crate__api__apply_filter_impl(ptr, rust_vec_len, data_len),
+        4 => wire__crate__api__apply_filter_timed_impl(ptr, rust_vec_len, data_len),
+        5 => wire__crate__api__begin_filter_impl(ptr, rust_vec_len, data_len),
+        6 => wire__crate__api__cancel_filter_impl(ptr, rust_vec_len, data_len),
+        7 => wire__crate__api__commit_filter_impl(ptr, rust_vec_len, data_len),
+        8 => wire__crate__api__current_image_impl(ptr, rust_vec_len, data_len),
+        9 => wire__crate__api__export_image_impl(ptr, rust_vec_len, data_len),
+        10 => wire__crate__api__flip_horizontal_impl(ptr, rust_vec_len, data_len),
+        11 => wire__crate__api__flip_vertical_impl(ptr, rust_vec_len, data_len),
+        12 => wire__crate__api__generate_filter_previews_impl(ptr, rust_vec_len, data_len),
+        13 => wire__crate__api__get_histogram_impl(ptr, rust_vec_len, data_len),
+        14 => wire__crate__api__load_image_impl(ptr, rust_vec_len, data_len),
+        15 => wire__crate__api__original_preview_impl(ptr, rust_vec_len, data_len),
+        16 => wire__crate__api__photon_filter_names_impl(ptr, rust_vec_len, data_len),
+        17 => wire__crate__api__prepare_preview_impl(ptr, rust_vec_len, data_len),
+        18 => wire__crate__api__redo_impl(ptr, rust_vec_len, data_len),
+        19 => wire__crate__api__resize_committed_impl(ptr, rust_vec_len, data_len),
+        20 => wire__crate__api__resize_image_impl(ptr, rust_vec_len, data_len),
+        21 => wire__crate__api__rotate_quarter_turns_impl(ptr, rust_vec_len, data_len),
+        22 => wire__crate__api__session_info_impl(ptr, rust_vec_len, data_len),
+        23 => wire__crate__api__straighten_impl(ptr, rust_vec_len, data_len),
+        24 => wire__crate__api__undo_impl(ptr, rust_vec_len, data_len),
+        25 => wire__crate__api__update_filter_preview_impl(ptr, rust_vec_len, data_len),
         _ => unreachable!(),
     }
 }
@@ -941,6 +1032,27 @@ impl flutter_rust_bridge::IntoIntoDart<crate::api::EditSessionInfo>
     for crate::api::EditSessionInfo
 {
     fn into_into_dart(self) -> crate::api::EditSessionInfo {
+        self
+    }
+}
+// Codec=Dco (DartCObject based), see doc to use other codecs
+impl flutter_rust_bridge::IntoDart for crate::api::FilterPreviewImage {
+    fn into_dart(self) -> flutter_rust_bridge::for_generated::DartAbi {
+        [
+            self.name.into_into_dart().into_dart(),
+            self.bytes.into_into_dart().into_dart(),
+        ]
+        .into_dart()
+    }
+}
+impl flutter_rust_bridge::for_generated::IntoDartExceptPrimitive
+    for crate::api::FilterPreviewImage
+{
+}
+impl flutter_rust_bridge::IntoIntoDart<crate::api::FilterPreviewImage>
+    for crate::api::FilterPreviewImage
+{
+    fn into_into_dart(self) -> crate::api::FilterPreviewImage {
         self
     }
 }
@@ -993,12 +1105,30 @@ impl SseEncode for f32 {
     }
 }
 
+impl SseEncode for crate::api::FilterPreviewImage {
+    // Codec=Sse (Serialization based), see doc to use other codecs
+    fn sse_encode(self, serializer: &mut flutter_rust_bridge::for_generated::SseSerializer) {
+        <String>::sse_encode(self.name, serializer);
+        <Vec<u8>>::sse_encode(self.bytes, serializer);
+    }
+}
+
 impl SseEncode for Vec<String> {
     // Codec=Sse (Serialization based), see doc to use other codecs
     fn sse_encode(self, serializer: &mut flutter_rust_bridge::for_generated::SseSerializer) {
         <i32>::sse_encode(self.len() as _, serializer);
         for item in self {
             <String>::sse_encode(item, serializer);
+        }
+    }
+}
+
+impl SseEncode for Vec<crate::api::FilterPreviewImage> {
+    // Codec=Sse (Serialization based), see doc to use other codecs
+    fn sse_encode(self, serializer: &mut flutter_rust_bridge::for_generated::SseSerializer) {
+        <i32>::sse_encode(self.len() as _, serializer);
+        for item in self {
+            <crate::api::FilterPreviewImage>::sse_encode(item, serializer);
         }
     }
 }
