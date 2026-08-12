6a53ad05b86bfaebdbab0e24a17bd1f0bd92a132
dexter-cnx <73522389+dexter-cnx@users.noreply.github.com>
2026-08-13 00:59:36 +0700

    Export Film Profile query API

--- packages/pixelcraft_film/lib/pixelcraft_film.dart
+++ packages/pixelcraft_film/lib/pixelcraft_film.dart
@@ -3,4 +3,5 @@ library;
 export 'src/film_profile_draft.dart';
 export 'src/film_profile_import_service.dart';
 export 'src/film_profile_library.dart';
+export 'src/film_profile_query.dart';
 export 'src/film_profile_repository.dart';
