dacb106867e4da59d0fa3f1388d6325d6a30d7fb
dexter-cnx <73522389+dexter-cnx@users.noreply.github.com>
2026-08-12 18:38:18 +0700

    P2: narrow Film Profile compatibility export

--- lib/core/film_profile_v1.dart
+++ lib/core/film_profile_v1.dart
@@ -1,1 +1,14 @@
-export 'package:pixelcraft_editing/pixelcraft_editing.dart';
+export 'package:pixelcraft_editing/pixelcraft_editing.dart'
+    show
+        pixelCraftProfileSchema,
+        pixelCraftProfileSchemaVersion,
+        pixelCraftProfileEngineVersion,
+        FilmProfileOrigin,
+        FilmProfileParameterSpec,
+        filmProfileParameterSpecs,
+        filmProfileParameterSpec,
+        FilmProfileV1,
+        FilmProfileMappingKind,
+        FilmProfileMapping,
+        FilmProfileImportReport,
+        importRecipeMap;
