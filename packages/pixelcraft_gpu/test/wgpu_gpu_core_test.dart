f55dbfe5de1f0bf8f714749b895eb1be6a98f896
dexter-cnx <73522389+dexter-cnx@users.noreply.github.com>
2026-08-12 16:56:59 +0700

    test(gpu): cover desktop wgpu ABI mask

---
+++ packages/pixelcraft_gpu/test/wgpu_gpu_core_test.dart
@@ -0,0 +1,20 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:pixelcraft_gpu/wgpu_gpu_core.dart';
+
+void main() {
+  test('decodes the stable wgpu backend ABI mask', () {
+    final backends = WgpuBackendSet.fromMask(0b1111);
+
+    expect(backends.vulkan, isTrue);
+    expect(backends.metal, isTrue);
+    expect(backends.dx12, isTrue);
+    expect(backends.gl, isTrue);
+    expect(backends.any, isTrue);
+  });
+
+  test('empty backend mask reports unavailable backend set', () {
+    final backends = WgpuBackendSet.fromMask(0);
+
+    expect(backends.any, isFalse);
+  });
+}
