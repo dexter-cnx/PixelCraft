9ab4b39102d834e6a33a9c68348f1fb658fd4ab6
dexter-cnx <73522389+dexter-cnx@users.noreply.github.com>
2026-08-18 06:55:46 +0700

    feat: request Gallery write permission at camera startup

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -1,8 +1,11 @@
 import Flutter
+import Photos
 import UIKit
 
 @main
 @objc class AppDelegate: FlutterAppDelegate, FlutterImplicitEngineDelegate {
+  private static let permissionChannel = "dev.cnxdev.pixelcraft/permissions"
+
   override func application(
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
@@ -12,5 +15,45 @@ import UIKit
 
   func didInitializeImplicitFlutterEngine(_ engineBridge: FlutterImplicitEngineBridge) {
     GeneratedPluginRegistrant.register(with: engineBridge.pluginRegistry)
+
+    let channel = FlutterMethodChannel(
+      name: Self.permissionChannel,
+      binaryMessenger: engineBridge.applicationRegistrar.messenger()
+    )
+    channel.setMethodCallHandler { call, result in
+      switch call.method {
+      case "requestGalleryWrite":
+        if #available(iOS 14, *) {
+          PHPhotoLibrary.requestAuthorization(for: .addOnly) { status in
+            DispatchQueue.main.async {
+              result(Self.permissionDecision(for: status))
+            }
+          }
+        } else {
+          PHPhotoLibrary.requestAuthorization { status in
+            DispatchQueue.main.async {
+              result(Self.permissionDecision(for: status))
+            }
+          }
+        }
+      default:
+        result(FlutterMethodNotImplemented)
+      }
+    }
+  }
+
+  private static func permissionDecision(for status: PHAuthorizationStatus) -> String {
+    switch status {
+    case .authorized, .limited:
+      return "granted"
+    case .restricted:
+      return "restricted"
+    case .denied:
+      return "restricted"
+    case .notDetermined:
+      return "denied"
+    @unknown default:
+      return "denied"
+    }
   }
 }
