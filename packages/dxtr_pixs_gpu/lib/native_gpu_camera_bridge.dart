87ed99581942b101b8538b986fb5b8420cc86e25
dexter-cnx <73522389+dexter-cnx@users.noreply.github.com>
2026-08-17 15:02:04 +0700

    Add native camera control contract

--- packages/dxtr_pixs_gpu/lib/native_gpu_camera_bridge.dart
+++ packages/dxtr_pixs_gpu/lib/native_gpu_camera_bridge.dart
@@ -8,6 +8,51 @@ typedef NativeGpuRuntimeFailureHandler = Future<void> Function(
   String message,
 );
 
+enum NativeCameraFlashMode { off, auto, on }
+
+extension NativeCameraFlashModeWire on NativeCameraFlashMode {
+  String get wireName => name;
+
+  static NativeCameraFlashMode parse(Object? value) => switch (value) {
+        'on' => NativeCameraFlashMode.on,
+        'off' => NativeCameraFlashMode.off,
+        _ => NativeCameraFlashMode.auto,
+      };
+}
+
+@immutable
+class NativeCameraControlState {
+  const NativeCameraControlState({
+    required this.lensDirection,
+    required this.hasFlash,
+    required this.hasTorch,
+    required this.flashMode,
+    required this.torchEnabled,
+    required this.mirrorEnabled,
+  });
+
+  factory NativeCameraControlState.fromMap(Map<Object?, Object?>? map) {
+    final value = map ?? const <Object?, Object?>{};
+    return NativeCameraControlState(
+      lensDirection: value['lensDirection'] as String? ?? 'back',
+      hasFlash: value['hasFlash'] as bool? ?? false,
+      hasTorch: value['hasTorch'] as bool? ?? false,
+      flashMode: NativeCameraFlashModeWire.parse(value['flashMode']),
+      torchEnabled: value['torchEnabled'] as bool? ?? false,
+      mirrorEnabled: value['mirrorEnabled'] as bool? ?? true,
+    );
+  }
+
+  final String lensDirection;
+  final bool hasFlash;
+  final bool hasTorch;
+  final NativeCameraFlashMode flashMode;
+  final bool torchEnabled;
+  final bool mirrorEnabled;
+
+  bool get isFront => lensDirection == 'front';
+}
+
 void _traceNativeCamera(String message) {
   if (kDebugMode) {
     debugPrint('[PF2][NativeCamera] $message');
@@ -79,6 +124,39 @@ class NativeGpuCameraBridge {
     }
   }
 
+  Future<NativeCameraControlState> controlState(String rendererId) =>
+      _control('cameraControlState', rendererId);
+
+  Future<NativeCameraControlState> setFlashMode(
+    String rendererId,
+    NativeCameraFlashMode mode,
+  ) =>
+      _control(
+        'setFlashMode',
+        rendererId,
+        <String, Object?>{'flashMode': mode.wireName},
+      );
+
+  Future<NativeCameraControlState> setTorchEnabled(
+    String rendererId,
+    bool enabled,
+  ) =>
+      _control(
+        'setTorchEnabled',
+        rendererId,
+        <String, Object?>{'enabled': enabled},
+      );
+
+  Future<NativeCameraControlState> setMirrorEnabled(
+    String rendererId,
+    bool enabled,
+  ) =>
+      _control(
+        'setMirrorEnabled',
+        rendererId,
+        <String, Object?>{'enabled': enabled},
+      );
+
   Future<String> capturePhoto(String rendererId) async {
     _traceNativeCamera('capturePhoto START rendererId=$rendererId');
     try {
@@ -123,6 +201,34 @@ class NativeGpuCameraBridge {
       _traceNativeCamera(
         'switchCamera FAILED rendererId=$rendererId error=$error',
       );
+      if (kDebugMode) debugPrintStack(stackTrace: stackTrace);
+      rethrow;
+    }
+  }
+
+  Future<NativeCameraControlState> _control(
+    String method,
+    String rendererId, [
+    Map<String, Object?> extra = const <String, Object?>{},
+  ]) async {
+    _traceNativeCamera('$method START rendererId=$rendererId');
+    try {
+      final result = await _channel.invokeMapMethod<Object?, Object?>(
+        method,
+        <String, Object?>{
+          'protocolVersion': gpuPreviewProtocolVersion,
+          'rendererId': rendererId,
+          ...extra,
+        },
+      );
+      final state = NativeCameraControlState.fromMap(result);
+      _traceNativeCamera(
+        '$method OK lens=${state.lensDirection} flash=${state.flashMode.name} '
+        'torch=${state.torchEnabled} mirror=${state.mirrorEnabled}',
+      );
+      return state;
+    } catch (error, stackTrace) {
+      _traceNativeCamera('$method FAILED rendererId=$rendererId error=$error');
       if (kDebugMode) debugPrintStack(stackTrace: stackTrace);
       rethrow;
     }
