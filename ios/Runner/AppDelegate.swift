94491f2b84240d7d7ff887b72165786683fd8165
dexter-cnx <73522389+dexter-cnx@users.noreply.github.com>
2026-08-18 10:02:20 +0700

    feat: expose latest Photos thumbnail to Flutter

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -25,8 +25,12 @@ import UIKit
       switch call.method {
       case "requestCamera":
         Self.requestCamera(result)
+      case "requestGalleryRead":
+        Self.requestGalleryRead(result)
       case "requestGalleryWrite":
         Self.requestGalleryWrite(result)
+      case "loadLatestGalleryThumbnail":
+        Self.loadLatestGalleryThumbnail(result)
       default:
         result(FlutterMethodNotImplemented)
       }
@@ -50,6 +54,22 @@ import UIKit
     }
   }
 
+  private static func requestGalleryRead(_ result: @escaping FlutterResult) {
+    if #available(iOS 14, *) {
+      PHPhotoLibrary.requestAuthorization(for: .readWrite) { status in
+        DispatchQueue.main.async {
+          result(permissionDecision(for: status))
+        }
+      }
+    } else {
+      PHPhotoLibrary.requestAuthorization { status in
+        DispatchQueue.main.async {
+          result(permissionDecision(for: status))
+        }
+      }
+    }
+  }
+
   private static func requestGalleryWrite(_ result: @escaping FlutterResult) {
     if #available(iOS 14, *) {
       PHPhotoLibrary.requestAuthorization(for: .addOnly) { status in
@@ -62,6 +82,47 @@ import UIKit
         DispatchQueue.main.async {
           result(permissionDecision(for: status))
         }
+      }
+    }
+  }
+
+  private static func loadLatestGalleryThumbnail(_ result: @escaping FlutterResult) {
+    let status: PHAuthorizationStatus
+    if #available(iOS 14, *) {
+      status = PHPhotoLibrary.authorizationStatus(for: .readWrite)
+    } else {
+      status = PHPhotoLibrary.authorizationStatus()
+    }
+    guard status == .authorized || status == .limited else {
+      result(nil)
+      return
+    }
+
+    let options = PHFetchOptions()
+    options.fetchLimit = 1
+    options.sortDescriptors = [NSSortDescriptor(key: "creationDate", ascending: false)]
+    let assets = PHAsset.fetchAssets(with: .image, options: options)
+    guard let asset = assets.firstObject else {
+      result(nil)
+      return
+    }
+
+    let requestOptions = PHImageRequestOptions()
+    requestOptions.deliveryMode = .highQualityFormat
+    requestOptions.resizeMode = .fast
+    requestOptions.isNetworkAccessAllowed = true
+    PHImageManager.default().requestImage(
+      for: asset,
+      targetSize: CGSize(width: 160, height: 160),
+      contentMode: .aspectFill,
+      options: requestOptions
+    ) { image, _ in
+      DispatchQueue.main.async {
+        guard let data = image?.jpegData(compressionQuality: 0.82) else {
+          result(nil)
+          return
+        }
+        result(FlutterStandardTypedData(bytes: data))
       }
     }
   }
