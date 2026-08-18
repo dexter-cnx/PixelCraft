9b06c545c1796993d7605c4ead92e58ee7421c4b
dexter-cnx <73522389+dexter-cnx@users.noreply.github.com>
2026-08-18 14:41:48 +0700

    fix: import platform view hit test behavior on iOS

--- packages/dxtr_pixs_gpu/lib/ios_gpu_camera_preview.dart
+++ packages/dxtr_pixs_gpu/lib/ios_gpu_camera_preview.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
 import 'package:flutter/services.dart';
 
 const iosGpuCameraPreviewViewType = 'dev.pixelcraft/gpu_camera_preview_v1';
