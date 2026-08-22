1a2a67373d3f7b8aa1b677f1e199d497df18de16
dexter-cnx <73522389+dexter-cnx@users.noreply.github.com>
2026-08-22 15:23:34 +0700

    test: avoid formatter-sensitive capture expression

--- test/state/native_gpu_camera_bridge_test.dart
+++ test/state/native_gpu_camera_bridge_test.dart
@@ -55,8 +55,8 @@ void main() {
       return <String, Object?>{'path': '/tmp/pixelcraft-camera/capture.jpg'};
     });
 
-    final capture = await const NativeGpuCameraBridge()
-        .capturePhoto('renderer-1');
+    const bridge = NativeGpuCameraBridge();
+    final capture = await bridge.capturePhoto('renderer-1');
     expect(capture.path, '/tmp/pixelcraft-camera/capture.jpg');
   });
 
